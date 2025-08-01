da9eba4aab91aa6393e93dd5b485cf0d0970c3ed
fagdjgbckdk <safdahh12331@gmail.com>
2025-08-01 20:53:17 +0600

    script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,26 @@
-bahdh
-shdb
-svdbn
-shdndn
+local player = game.Players.LocalPlayer
+local humanoid = player.Character:WaitForChild("Humanoid")
+local jumpEnabled = false
+local currentJumpPower = 50
+
+local function updateJump()
+    if jumpEnabled then
+        humanoid.JumpPower = currentJumpPower
+    else
+        humanoid.JumpPower = 50
+    end
+end
+
+game:GetService("UserInputService").InputBegan:Connect(function(key, gpe)
+    if gpe then return end
+    if key.KeyCode == Enum.KeyCode.J then
+        jumpEnabled = not jumpEnabled
+        updateJump()
+    elseif key.KeyCode == Enum.KeyCode.K then
+        currentJumpPower = currentJumpPower + 10
+        updateJump()
+    elseif key.KeyCode == Enum.KeyCode.L then
+        currentJumpPower = math.max(50, currentJumpPower - 10)
+        updateJump()
+    end
+end)
